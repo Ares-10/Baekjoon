9146f6000f4db1559e51a5a9c413a066b9017c90
Ares-10 <115702474+Ares-10@users.noreply.github.com>
2025-05-11 17:33:15 +0900

    [level 1] Title: 폰켓몬, Time: 1.48 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <set>
+#include <cmath>
+
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    set<int> s;
+    for (int i = 0; i < nums.size(); i++)
+        s.insert(nums[i]);
+    return min(s.size(), nums.size() / 2);
+}
