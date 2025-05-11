9b4fafb71b281242ae2f698120835c9e0af349e0
Ares-10 <115702474+Ares-10@users.noreply.github.com>
2025-05-11 21:20:20 +0900

    [level 2] Title: 올바른 괄호, Time: 0.69 ms, Memory: 3.87 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    
+    stack<int> st;
+    
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '(')
+            st.push('(');
+        else
+        {
+            if (st.size() != 0)
+                st.pop();
+            else
+                return false;
+        }
+    }
+    if (st.size() != 0)
+        return false;
+    return true;
+}
